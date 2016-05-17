622d33546816b3290e4ae6262e7a2b645bff4492
Samuel Relton <samuel.relton@manchester.ac.uk>
2016-05-17 11:56:28 +0100

    Added working version of zsymm using OpenMP.

---
+++ include/core_blas_z.h
@@ -0,0 +1,73 @@
+/**
+ *
+ * @file core_blas_z.h
+ *
+ *  PLASMA header.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @date 2016-01-01
+ * @precisions normal z -> c d s
+ *
+ **/
+#ifndef ICL_CORE_BLAS_Z_H
+#define ICL_CORE_BLAS_Z_H
+
+#include "plasma_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+void CORE_zgemm(
+    PLASMA_enum transA, PLASMA_enum transB,
+    int m, int n, int k,
+    PLASMA_Complex64_t alpha, const PLASMA_Complex64_t *A, int lda,
+                               const PLASMA_Complex64_t *B, int ldb,
+    PLASMA_Complex64_t beta,        PLASMA_Complex64_t *C, int ldc);
+
+void CORE_zsymm(
+    PLASMA_enum side, PLASMA_enum uplo,
+    int m, int n,
+    PLASMA_Complex64_t alpha, const PLASMA_Complex64_t *A, int lda,
+                               const PLASMA_Complex64_t *B, int ldb,
+    PLASMA_Complex64_t beta,        PLASMA_Complex64_t *C, int ldc);
+
+void CORE_zlacpy(
+    PLASMA_enum uplo,
+    int m, int n,
+    const PLASMA_Complex64_t *A, int lda,
+          PLASMA_Complex64_t *B, int ldb);
+
+/******************************************************************************/
+void CORE_OMP_zgemm(
+    PLASMA_enum transA, PLASMA_enum transB,
+    int m, int n, int k,
+    PLASMA_Complex64_t alpha, const PLASMA_Complex64_t *A, int lda,
+                               const PLASMA_Complex64_t *B, int ldb,
+    PLASMA_Complex64_t beta,        PLASMA_Complex64_t *C, int ldc);
+
+
+void CORE_OMP_zsymm(
+    PLASMA_enum side, PLASMA_enum uplo,
+    int m, int n,
+    PLASMA_Complex64_t alpha, const PLASMA_Complex64_t *A, int lda,
+                               const PLASMA_Complex64_t *B, int ldb,
+    PLASMA_Complex64_t beta,        PLASMA_Complex64_t *C, int ldc);
+
+
+void CORE_OMP_zlacpy(
+    PLASMA_enum uplo,
+    int m, int n, int nb,
+    const PLASMA_Complex64_t *A, int lda,
+          PLASMA_Complex64_t *B, int ldb);
+
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif // ICL_CORE_BLAS_Z_H
---
+++ include/plasma_internal_z.h
@@ -0,0 +1,55 @@
+/**
+ *
+ * @file plasma_internal_z.h
+ *
+ *  PLASMA control routines.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @author Samuel D. Relton
+ * @date 2016-05-16
+ * @precisions normal z -> s d c
+ *
+ **/
+#ifndef ICL_PLASMA_INTERNAL_Z_H
+#define ICL_PLASMA_INTERNAL_Z_H
+
+#include "plasma_async.h"
+#include "plasma_descriptor.h"
+#include "plasma_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+void plasma_pzgemm(
+    PLASMA_enum transA, PLASMA_enum transB,
+    PLASMA_Complex64_t alpha, PLASMA_desc A,
+                              PLASMA_desc B,
+    PLASMA_Complex64_t beta,  PLASMA_desc C,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzsymm(PLASMA_enum side, PLASMA_enum uplo,
+				   PLASMA_Complex64_t alpha, PLASMA_desc A,
+				                             PLASMA_desc B,
+				   PLASMA_Complex64_t beta,  PLASMA_desc C,
+				   PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzooccrb2cm(
+    PLASMA_desc A,
+    PLASMA_Complex64_t *Af77, int lda,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzoocm2ccrb(
+    PLASMA_Complex64_t *Af77, int lda,
+    PLASMA_desc A,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif // ICL_PLASMA_INTERNAL_Z_H
