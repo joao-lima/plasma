7b293e959678a683e69034f69ef551775ccb8699
Pedro Valero-Lara <pedro.valero-lara@manchester.ac.uk>
2016-05-23 09:59:53 +0100

    Herk and Syrk implemented in OpenMP

---
+++ include/plasma_async.h
@@ -0,0 +1,47 @@
+/**
+ *
+ * @file plasma_async.h
+ *
+ *  PLASMA control routines.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @date 2016-01-01
+ *
+ **/
+#ifndef ICL_PLASMA_ASYNC_H
+#define ICL_PLASMA_ASYNC_H
+
+#include "plasma_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+typedef struct {
+    PLASMA_bool status; ///< error code
+} PLASMA_request;
+
+static const PLASMA_request PLASMA_REQUEST_INITIALIZER = {PLASMA_SUCCESS};
+
+typedef struct {
+    PLASMA_bool status;      ///< error code
+    PLASMA_request *request; ///< failed request
+} PLASMA_sequence;
+
+/******************************************************************************/
+int plasma_request_fail(PLASMA_sequence *sequence,
+                        PLASMA_request *request,
+                        int status);
+
+int plasma_sequence_create(PLASMA_sequence **sequence);
+int plasma_sequence_destroy(PLASMA_sequence *sequence);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif // ICL_PLASMA_ASYNC_H
---
+++ include/plasma_internal_z.h
@@ -0,0 +1,65 @@
+/**
+ *
+ * @file plasma_internal_z.h
+ *
+ *  PLASMA control routines.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley, Univ. of Colorado Denver and
+ *  Univ. of Manchester.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @author Mawussi Zounon
+ * @author Pedro V. Lara
+ * @author Maksims Abalenkovs 
+ * @author Samuel D. Relton
+ * @date 2016-05-11
+ * @precisions normal z -> s d c
+ *
+ **/
+#ifndef ICL_PLASMA_INTERNAL_Z_H
+#define ICL_PLASMA_INTERNAL_Z_H
+
+#include "plasma_async.h"
+#include "plasma_descriptor.h"
+#include "plasma_types.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+void plasma_pzgemm(
+    PLASMA_enum transA, PLASMA_enum transB,
+    PLASMA_Complex64_t alpha, PLASMA_desc A,
+                              PLASMA_desc B,
+    PLASMA_Complex64_t beta,  PLASMA_desc C,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzsyrk(
+    PLASMA_enum uplo, PLASMA_enum trans,
+    PLASMA_Complex64_t alpha, PLASMA_desc A,
+    PLASMA_Complex64_t beta,  PLASMA_desc C,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzherk(
+    PLASMA_enum uplo, PLASMA_enum trans,
+    double alpha, PLASMA_desc A,
+    double beta,  PLASMA_desc C,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzooccrb2cm(
+    PLASMA_desc A,
+    PLASMA_Complex64_t *Af77, int lda,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+void plasma_pzoocm2ccrb(
+    PLASMA_Complex64_t *Af77, int lda,
+    PLASMA_desc A,
+    PLASMA_sequence *sequence, PLASMA_request *request);
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
+
+#endif // ICL_PLASMA_INTERNAL_Z_H
