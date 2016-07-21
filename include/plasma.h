a425741584868dffbd9861edb56017b2cb0089b1
Jakub Kurzak <jacob@cofi.tech>
2016-07-21 12:08:52 -0400

    pointing to the last release of old PLASMA on Bitbucket (downloads)

---
+++ include/plasma.h
@@ -0,0 +1,33 @@
+/**
+ *
+ * @file plasma.h
+ *
+ *  PLASMA header.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @date 2016-01-01
+ *
+ **/
+#ifndef ICL_PLASMA_H
+#define ICL_PLASMA_H
+
+#ifdef PLASMA_WITH_MKL
+    #include <mkl.h>
+#else
+    #include <cblas.h>
+    #include <lapacke.h>
+#endif
+
+#include "plasma_async.h"
+#include "plasma_descriptor.h"
+#include "plasma_context.h"
+
+#include "plasma_s.h"
+#include "plasma_d.h"
+#include "plasma_c.h"
+#include "plasma_z.h"
+
+#endif // ICL_PLASMA_H
