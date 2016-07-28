b6027fb4676112d2d7a8915ba0d16399d07d5049
zounon <mawussi.zounon@gmail.com>
2016-07-28 15:30:54 +0100

    Adjusting #pragma indentation.

---
+++ include/plasma_tune.h
@@ -0,0 +1,24 @@
+/**
+ *
+ * @file plasma_tune.h
+ *
+ *  PLASMA control routines.
+ *  PLASMA is a software package provided by Univ. of Tennessee,
+ *  Univ. of California Berkeley and Univ. of Colorado Denver.
+ *
+ * @version 3.0.0
+ * @author Jakub Kurzak
+ * @date 2016-01-01
+ *
+ **/
+#ifndef ICL_PLASMA_TUNE_H
+#define ICL_PLASMA_TUNE_H
+
+enum {
+    PLASMA_FUNC_SGEMM,
+    PLASMA_FUNC_DGEMM,
+    PLASMA_FUNC_CGEMM,
+    PLASMA_FUNC_ZGEMM
+};
+
+#endif // ICL_PLASMA_TUNE_H
