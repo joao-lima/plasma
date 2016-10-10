eb0f191daf5ce4e6dcda469ffa50267d32d5b6d6
Maksims Abalenkovs <m.abalenkovs@manchester.ac.uk>
2016-10-10 15:40:10 +0100

    * xTRADD routines were brought up to date with newest PLASMA conventions.

---
+++ core_blas/core_ztradd.c
@@ -0,0 +1,222 @@
+/**
+ *
+ * @file
+ *
+ *  PLASMA is a software package provided by:
+ *  University of Tennessee, US,
+ *  University of Manchester, UK.
+ *
+ * @precisions normal z -> c d s
+ *
+ **/
+
+#include "core_blas.h"
+#include "plasma_internal.h"
+#include "plasma_types.h"
+#include "core_lapack.h"
+
+/***************************************************************************//**
+ *
+ * @ingroup core_tradd
+ *
+ *  Performs an addition of two trapezoidal matrices similarly to the
+ * 'pztradd()' function from the PBLAS library:
+ *
+ *    \f[ B = \alpha * op( A ) + \beta * B, \f]
+ *
+ *  where op( X ) is one of:
+ *    \f[ op( X ) = X,   \f]
+ *    \f[ op( X ) = X^T, \f]
+ *    \f[ op( X ) = X^H, \f]
+ *
+ *  alpha and beta are scalars and A, B are matrices with op( A ) an m-by-n or
+ *  n-by-m matrix depending on the value of transa and B an m-by-n matrix.
+ *
+ *******************************************************************************
+ *
+ * @param[in] uplo
+ *          Specifies the shape of A and B matrices:
+ *          - PlasmaGeneral: A and B are general matrices.
+ *          - PlasmaUpper:   op( A ) and B are upper trapezoidal matrices.
+ *          - PlasmaLower:   op( A ) and B are lower trapezoidal matrices.
+ *
+ * @param[in] transa
+ *          Specifies whether the matrix A is non-transposed, transposed, or
+ *          conjugate transposed
+ *          - PlasmaNoTrans:   op( A ) = A
+ *          - PlasmaTrans:     op( A ) = A^T
+ *          - PlasmaConjTrans: op( A ) = A^H
+ *
+ * @param[in] m
+ *          Number of rows of the matrices op( A ) and B.
+ *          m >= 0.
+ *
+ * @param[in] n
+ *          Number of columns of the matrices op( A ) and B.
+ *          n >= 0.
+ *
+ * @param[in] alpha
+ *          Scalar factor of A.
+ *
+ * @param[in] A
+ *          Matrix of size lda-by-k, where k is n when transa == PlasmaNoTrans
+ *          and m otherwise.
+ *
+ * @param[in] lda
+ *          Leading dimension of the array A. lda >= max(1,l), where l is m
+ *          when transa = PlasmaNoTrans and n otherwise.
+ *
+ * @param[in] beta
+ *          Scalar factor of B.
+ *
+ * @param[in,out] B
+ *          Matrix of size ldb-by-n.
+ *          On exit, B = alpha * op( A ) + beta * B
+ *
+ * @param[in] ldb
+ *          Leading dimension of the array B.
+ *          ldb >= max(1,m).
+ *
+ ******************************************************************************/
+void core_ztradd(plasma_enum_t uplo, plasma_enum_t transa, int m, int n,
+                       plasma_complex64_t  alpha,
+                 const plasma_complex64_t *A, int lda,
+                       plasma_complex64_t  beta,
+                       plasma_complex64_t *B, int ldb)
+{
+    int i, j;
+
+    if (uplo == PlasmaGeneral) {
+        core_zgeadd(transa, m, n, alpha, A, lda, beta, B, ldb);
+        return;
+    }
+
+    if ((uplo != PlasmaUpper) &&
+        (uplo != PlasmaLower)) {
+        coreblas_error("illegal value of uplo");
+        return;
+    }
+
+    if ((transa != PlasmaNoTrans) &&
+        (transa != PlasmaTrans)   &&
+        (transa != PlasmaConjTrans)) {
+
+        coreblas_error("illegal value of transa");
+        return;
+    }
+
+    if (m < 0) {
+        coreblas_error("Illegal value of m");
+        return;
+    }
+
+    if (n < 0) {
+        coreblas_error("Illegal value of m");
+        return;
+    }
+
+    if (A == NULL) {
+        coreblas_error("NULL A");
+        return;
+    }
+
+    if ( ((transa == PlasmaNoTrans) && (lda < imax(1,m)) && (m > 0)) ||
+         ((transa != PlasmaNoTrans) && (lda < imax(1,n)) && (n > 0)) ) {
+
+        coreblas_error("Illegal value of lda");
+        return;
+    }
+
+    if (B == NULL) {
+        coreblas_error("NULL B");
+        return;
+    }
+
+    if ( (ldb < imax(1,m)) && (m > 0) ) {
+        coreblas_error("Illegal value of ldb");
+        return;
+    }
+
+    //=============
+    // PlasmaLower
+    //=============
+    if (uplo == PlasmaLower) {
+        switch (transa) {
+        case PlasmaConjTrans:
+            for (j = 0; j < n; j++, A++) {
+                for (i = j; i < m; i++, B++) {
+                    *B = beta * (*B) + alpha * conj(A[lda*i]);
+                }
+                B += ldb-m+j+1;
+            }
+            break;
+
+        case PlasmaTrans:
+            for (j = 0; j < n; j++, A++) {
+                for (i = j; i < m; i++, B++) {
+                    *B = beta * (*B) + alpha * A[lda*i];
+                }
+                B += ldb-m+j+1;
+            }
+            break;
+
+        case PlasmaNoTrans:
+        default:
+            for (j = 0; j < n; j++) {
+                for (i = j; i < m; i++, B++, A++) {
+                    *B = beta * (*B) + alpha * (*A);
+                }
+                B += ldb-m+j+1;
+                A += lda-m+j+1;
+            }
+        }
+    }
+    //=============
+    // PlasmaUpper
+    //=============
+    else {
+        switch (transa) {
+        case PlasmaConjTrans:
+            for (j = 0; j < n; j++, A++) {
+                int mm = imin( j+1, m );
+                for (i = 0; i < mm; i++, B++) {
+                    *B = beta * (*B) + alpha * conj(A[lda*i]);
+                }
+                B += ldb-mm;
+            }
+            break;
+
+        case PlasmaTrans:
+            for (j = 0; j < n; j++, A++) {
+                int mm = imin( j+1, m );
+                for (i = 0; i < mm; i++, B++) {
+                    *B = beta * (*B) + alpha * (A[lda*i]);
+                }
+                B += ldb-mm;
+            }
+            break;
+
+        case PlasmaNoTrans:
+        default:
+            for (j = 0; j < n; j++) {
+                int mm = imin( j+1, m );
+                for (i = 0; i < mm; i++, B++, A++) {
+                    *B = beta * (*B) + alpha * (*A);
+                }
+                B += ldb-mm;
+                A += lda-mm;
+            }
+        }
+    }
+}
+
+/******************************************************************************/
+void core_omp_ztradd(
+    plasma_enum_t uplo, plasma_enum_t transa, int m, int n,
+    plasma_complex64_t alpha, const plasma_complex64_t *A, int lda,
+    plasma_complex64_t beta,        plasma_complex64_t *B, int ldb)
+{
+    // omp depend assumes lda = PlasmaNoTrans ? m : n; ldb = m
+    #pragma omp task depend(in:A[0:m*n]) depend(inout:B[0:m*n])
+    core_ztradd(uplo, transa, m, n, alpha, A, lda, beta, B, ldb);
+}
---
+++ core_blas/core_ztsmlq.c
@@ -0,0 +1,292 @@
+/**
+ *
+ * @file
+ *
+ *  PLASMA is a software package provided by:
+ *  University of Tennessee, US,
+ *  University of Manchester, UK.
+ *
+ * @precisions normal z -> c d s
+ *
+ **/
+
+#include "core_blas.h"
+#include "plasma_types.h"
+#include "plasma_internal.h"
+#include "core_lapack.h"
+
+#include <omp.h>
+
+/***************************************************************************//**
+ *
+ * @ingroup core_tsmlq
+ *
+ *  Overwrites the general complex m1-by-n1 tile A1 and
+ *  m2-by-n2 tile A2 with
+ *
+ *                                side = PlasmaLeft        side = PlasmaRight
+ *    trans = PlasmaNoTrans            Q * | A1 |           | A1 A2 | * Q
+ *                                         | A2 |
+ *
+ *    trans = Plasma_ConjTrans       Q^H * | A1 |           | A1 A2 | * Q^H
+ *                                         | A2 |
+ *
+ *  where Q is a complex unitary matrix defined as the product of k
+ *  elementary reflectors
+ *
+ *    Q = H(k)^H . . . H(2)^H H(1)^H
+ *
+ *  as returned by core_ztslqt.
+ *
+ *******************************************************************************
+ *
+ * @param[in] side
+ *         - PlasmaLeft  : apply Q or Q^H from the Left;
+ *         - PlasmaRight : apply Q or Q^H from the Right.
+ *
+ * @param[in] trans
+ *         @arg PlasmaNoTrans   :  Apply Q;
+ *         @arg Plasma_ConjTrans:  Apply Q^H.
+ *
+ * @param[in] m1
+ *         The number of rows of the tile A1. m1 >= 0.
+ *
+ * @param[in] n1
+ *         The number of columns of the tile A1. n1 >= 0.
+ *
+ * @param[in] m2
+ *         The number of rows of the tile A2. m2 >= 0.
+ *         m2 = m1 if side == PlasmaRight.
+ *
+ * @param[in] n2
+ *         The number of columns of the tile A2. n2 >= 0.
+ *         n2 = n1 if side == PlasmaLeft.
+ *
+ * @param[in] k
+ *         The number of elementary reflectors whose product defines
+ *         the matrix Q.
+ *
+ * @param[in] ib
+ *         The inner-blocking size.  ib >= 0.
+ *
+ * @param[in,out] A1
+ *         On entry, the m1-by-n1 tile A1.
+ *         On exit, A1 is overwritten by the application of Q.
+ *
+ * @param[in] lda1
+ *         The leading dimension of the array A1. lda1 >= max(1,m1).
+ *
+ * @param[in,out] A2
+ *         On entry, the m2-by-n2 tile A2.
+ *         On exit, A2 is overwritten by the application of Q.
+ *
+ * @param[in] lda2
+ *         The leading dimension of the tile A2. lda2 >= max(1,m2).
+ *
+ * @param[in] V
+ *         The i-th row must contain the vector which defines the
+ *         elementary reflector H(i), for i = 1,2,...,k, as returned by
+ *         core_ztslqt in the first k rows of its array argument V.
+ *
+ * @param[in] ldv
+ *         The leading dimension of the array V. ldv >= max(1,k).
+ *
+ * @param[in] T
+ *         The ib-by-k triangular factor T of the block reflector.
+ *         T is upper triangular by block (economic storage);
+ *         The rest of the array is not referenced.
+ *
+ * @param[in] ldt
+ *         The leading dimension of the array T. ldt >= ib.
+ *
+ * @param WORK
+ *         Auxiliary workspace array of length
+ *             ldwork-by-m1 if side == PlasmaLeft
+ *             ldwork-by-ib if side == PlasmaRight
+ *
+ * @param[in] ldwork
+ *         The leading dimension of the array WORK.
+ *             ldwork >= max(1,ib) if side == PlasmaLeft
+ *             ldwork >= max(1,n1) if side == PlasmaRight
+ *
+ *******************************************************************************
+ *
+ * @retval PlasmaSuccess successful exit
+ * @retval < 0 if -i, the i-th argument had an illegal value
+ *
+ ******************************************************************************/
+int core_ztsmlq(plasma_enum_t side, plasma_enum_t trans,
+                int m1, int n1, int m2, int n2, int k, int ib,
+                      plasma_complex64_t *A1, int lda1,
+                      plasma_complex64_t *A2, int lda2,
+                const plasma_complex64_t *V, int ldv,
+                const plasma_complex64_t *T, int ldt,
+                      plasma_complex64_t *WORK, int ldwork)
+{
+    int i, i1, i3;
+    int nw;
+    int kb;
+    int ic = 0;
+    int jc = 0;
+    int mi = m1;
+    int ni = n1;
+
+    // Check input arguments
+    if ((side != PlasmaLeft) && (side != PlasmaRight)) {
+        coreblas_error("Illegal value of side");
+        return -1;
+    }
+
+    // nw is the minimum dimension of WORK
+    if (side == PlasmaLeft) {
+        nw = ib;
+    }
+    else {
+        nw = n1;
+    }
+
+    if ((trans != PlasmaNoTrans) && (trans != Plasma_ConjTrans)) {
+        coreblas_error("Illegal value of trans");
+        return -2;
+    }
+    if (m1 < 0) {
+        coreblas_error("Illegal value of m1");
+        return -3;
+    }
+    if (n1 < 0) {
+        coreblas_error("Illegal value of n1");
+        return -4;
+    }
+    if ((m2 < 0) ||
+        ((m2 != m1) && (side == PlasmaRight))) {
+        coreblas_error("Illegal value of m2");
+        return -5;
+    }
+    if ((n2 < 0) ||
+        ((n2 != n1) && (side == PlasmaLeft))) {
+        coreblas_error("Illegal value of n2");
+        return -6;
+    }
+    if ((k < 0) ||
+        ( (side == PlasmaLeft)  && (k > m1) ) ||
+        ( (side == PlasmaRight) && (k > n1) ) ) {
+        coreblas_error("Illegal value of k");
+        return -7;
+    }
+    if (ib < 0) {
+        coreblas_error("Illegal value of ib");
+        return -8;
+    }
+    if (lda1 < imax(1,m1)) {
+        coreblas_error("Illegal value of lda1");
+        return -10;
+    }
+    if (lda2 < imax(1,m2)) {
+        coreblas_error("Illegal value of lda2");
+        return -12;
+    }
+    if (ldv < imax(1,k)) {
+        coreblas_error("Illegal value of ldv");
+        return -14;
+    }
+    if (ldt < imax(1,ib)) {
+        coreblas_error("Illegal value of ldt");
+        return -16;
+    }
+    if (ldwork < imax(1,nw)) {
+        coreblas_error("Illegal value of ldwork");
+        return -18;
+    }
+
+    // Quick return
+    if ((m1 == 0) || (n1 == 0) ||
+        (m2 == 0) || (n2 == 0) ||
+        (k == 0)  || (ib == 0))
+        return PlasmaSuccess;
+
+    if (((side == PlasmaLeft) && (trans == PlasmaNoTrans))
+        || ((side == PlasmaRight) && (trans != PlasmaNoTrans))) {
+        i1 = 0;
+        i3 = ib;
+    }
+    else {
+        i1 = ((k-1) / ib)*ib;
+        i3 = -ib;
+    }
+
+    if (trans == PlasmaNoTrans) {
+        trans = Plasma_ConjTrans;
+    }
+    else {
+        trans = PlasmaNoTrans;
+    }
+
+    for (i = i1; (i > -1) && (i < k); i += i3) {
+        kb = imin(ib, k-i);
+
+        if (side == PlasmaLeft) {
+            // H or H^H is applied to C(i:m,1:n)
+            mi = m1 - i;
+            ic = i;
+        }
+        else {
+            // H or H^H is applied to C(1:m,i:n)
+            ni = n1 - i;
+            jc = i;
+        }
+        // Apply H or H^H (NOTE: core_zparfb used to be core_ztsrfb)
+        core_zparfb(
+            side, trans, PlasmaForward, PlasmaRowwise,
+            mi, ni, m2, n2, kb, 0,
+            &A1[lda1*jc+ic], lda1,
+            A2, lda2,
+            &V[i], ldv,
+            &T[ldt*i], ldt,
+            WORK, ldwork);
+    }
+
+    return PlasmaSuccess;
+}
+
+/******************************************************************************/
+void core_omp_ztsmlq(plasma_enum_t side, plasma_enum_t trans,
+                     int m1, int n1, int m2, int n2, int k, int ib, int nb,
+                           plasma_complex64_t *A1, int lda1,
+                           plasma_complex64_t *A2, int lda2,
+                     const plasma_complex64_t *V,  int ldv,
+                     const plasma_complex64_t *T,  int ldt,
+                     plasma_workspace_t work,
+                     plasma_sequence_t *sequence, plasma_request_t *request)
+{
+    // OpenMP depends assume m1 == n1 == m2 == n2 == lda1 == lda2 == ldv == nb,
+    //                       ldt == ib.
+    #pragma omp task depend(inout:A1[0:nb*nb]) \
+                     depend(inout:A2[0:nb*nb]) \
+                     depend(in:V[0:nb*nb]) \
+                     depend(in:T[0:ib*nb])
+    {
+        if (sequence->status == PlasmaSuccess) {
+            int tid = omp_get_thread_num();
+            plasma_complex64_t *W   =
+                ((plasma_complex64_t*)work.spaces[tid]);
+
+            int ldwork = side == PlasmaLeft ? ib : nb;
+
+            // call the kernel
+            int info = core_ztsmlq(side, trans,
+                                   m1, n1, m2, n2, k, ib,
+                                   A1, lda1,
+                                   A2, lda2,
+                                   V, ldv,
+                                   T, ldt,
+                                   W, ldwork);
+
+            if (info != PlasmaSuccess) {
+                plasma_error_with_code("Error in call to COREBLAS in argument",
+                                       -info);
+                plasma_request_fail(sequence, request,
+                                    PlasmaErrorIllegalValue);
+            }
+        }
+    }
+}
