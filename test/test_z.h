c4690dff3c7add8f3149b88a71ee3ef5062d5885
Jakub Kurzak <jacob@cofi.tech>
2016-11-16 20:17:33 -0500

    forgotten barrier for LU

---
+++ test/test_z.h
@@ -0,0 +1,46 @@
+/**
+ *
+ * @file
+ *
+ *  PLASMA is a software package provided by:
+ *  University of Tennessee, US,
+ *  University of Manchester, UK.
+ *
+ * @precisions normal z -> s d c
+ *
+ **/
+#ifndef TEST_Z_H
+#define TEST_Z_H
+
+#include "test.h"
+
+//==============================================================================
+// test routines
+//==============================================================================
+void test_zgeadd(param_value_t param[], char *info);
+void test_zgelqf(param_value_t param[], char *info);
+void test_zgelqs(param_value_t param[], char *info);
+void test_zgels(param_value_t param[], char *info);
+void test_zgemm(param_value_t param[], char *info);
+void test_zgeqrf(param_value_t param[], char *info);
+void test_zgeqrs(param_value_t param[], char *info);
+void test_zgetrf(param_value_t param[], char *info);
+void test_zhemm(param_value_t param[], char *info);
+void test_zher2k(param_value_t param[], char *info);
+void test_zherk(param_value_t param[], char *info);
+void test_zpbsv(param_value_t param[], char *info);
+void test_zpbtrf(param_value_t param[], char *info);
+void test_zposv(param_value_t param[], char *info);
+void test_zpotrf(param_value_t param[], char *info);
+void test_zpotrs(param_value_t param[], char *info);
+void test_zsymm(param_value_t param[], char *info);
+void test_zsyr2k(param_value_t param[], char *info);
+void test_zsyrk(param_value_t param[], char *info);
+void test_ztradd(param_value_t param[], char *info);
+void test_ztrmm(param_value_t param[], char *info);
+void test_ztrsm(param_value_t param[], char *info);
+void test_ztrtri(param_value_t param[], char *info);
+void test_zunmlq(param_value_t param[], char *info);
+void test_zunmqr(param_value_t param[], char *info);
+
+#endif // TEST_Z_H
